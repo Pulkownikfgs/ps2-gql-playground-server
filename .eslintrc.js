d03d49917b2cc69290c3abe28853f09503f95ff0
Alexander Polyakov <alexplkv@yandex.ru>
2021-06-17 20:16:15 +0300

    linters import order

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,19 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
-  rules: {},
+  rules: {
+    'import/order': ['error'],
+  },
 };
